b41410de298de293a91f6cd9994556e21ba9df38
molexandev01~ <molexandev@gmail.com>
2024-06-24 11:37:01 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,36 @@
+$(function () {});
+
+// // Функція для прокрутки сторінки наверх
+// function scrollToTop() {
+//    // Прокрутити сторінку до верху з плавністю
+//    window.scrollTo({
+//       top: 0,
+//       behavior: "smooth",
+//    });
+// }
+
+// // Отримати елемент кнопки
+// var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// // Додати обробник подій для події прокрутки
+// window.addEventListener("scroll", function () {
+//    // Визначити, чи користувач почав скролити сторінку
+//    if (
+//       document.body.scrollTop > 93 ||
+//       document.documentElement.scrollTop > 93
+//    ) {
+//       scrollToTopBtn.style.display = "flex";
+//    } else {
+//       scrollToTopBtn.style.display = "none";
+//    }
+// });
+
+// // wow animation
+// wow = new WOW({
+//    boxClass: "wow",
+//    animateClass: "animate__animated",
+//    offset: 25,
+//    mobile: true,
+//    live: true,
+// });
+// wow.init();
