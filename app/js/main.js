4206bb26486ec50b0ed001135304279c8ef17598
molexandev01~ <molexandev@gmail.com>
2024-09-24 19:10:56 +0300

    added styles for few sections

--- app/js/main.js
+++ app/js/main.js
@@ -20,6 +20,48 @@ $(function () {
       autoplaySpeed: 3000,
       draggable: true,
    });
+
+   //
+
+   // $('').slick({
+
+   // });
+
+   $('.testimonials__slider-inner')
+      .on('init', function (event, slick) {
+         // Додаємо клас до наступного слайда після активного
+         $('.slick-slide[data-slick-index="1"]').addClass('next-slide');
+      })
+      .slick({
+         dots: true,
+         arrows: false,
+         speed: 500,
+         autoplay: true,
+         autoplaySpeed: 2000,
+         draggable: true,
+         slidesToShow: 2,
+      });
+
+   $('.testimonials__slider-inner').on(
+      'beforeChange',
+      function (event, slick, currentSlide, nextSlide) {
+         // Видаляємо клас з усіх слайдів
+         $('.slick-slide').removeClass('next-slide');
+
+         // Додаємо клас до наступного слайда під час прокрутки
+         $('.slick-slide[data-slick-index="' + (nextSlide + 1) + '"]').addClass(
+            'next-slide'
+         );
+      }
+   );
+
+   $('.faq__item-title').on('click', function () {
+      $(this).toggleClass('faq__item-title--active');
+      $(this).next().slideToggle();
+   });
+   $('.faq__item').on('click', function () {
+      $(this).toggleClass('faq__item--active');
+   });
 });
 
 // Функція для прокрутки сторінки наверх
